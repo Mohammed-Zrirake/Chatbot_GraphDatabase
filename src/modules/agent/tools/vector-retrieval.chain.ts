d7f0fff8831932e127fe0bd3ba75bfaa1ea121b2
mohammed.Zr <mohamed.zrirake@gmail.com>
2025-09-07 16:50:12 +0100

    Cypher Retrieval Tool

--- src/modules/agent/tools/vector-retrieval.chain.ts
+++ src/modules/agent/tools/vector-retrieval.chain.ts
@@ -5,45 +5,71 @@ import {
 } from "@langchain/core/runnables";
 import { Embeddings } from "@langchain/core/embeddings";
 import initGenerateAnswerChain from "../chains/answer-generation.chain";
-import { BaseLanguageModel } from "langchain/base_language";
+import { BaseLanguageModel } from "@langchain/core/language_models/base";
 import initVectorStore from "../vector.store";
 import { saveHistory } from "../history";
 import { DocumentInterface } from "@langchain/core/documents";
-import { AgentToolInput } from "../agent.types";
 
-// tag::throughput[]
+
+
 type RetrievalChainThroughput = AgentToolInput & {
   context: string;
   output: string;
   ids: string[];
 };
-// end::throughput[]
 
-// tag::extractDocumentIds[]
-// Helper function to extract document IDs from Movie node metadata
+
+
+
 const extractDocumentIds = (
   documents: DocumentInterface<{ _id: string; [key: string]: any }>[]
 ): string[] => documents.map((document) => document.metadata._id);
-// end::extractDocumentIds[]
 
-// tag::docsToJson[]
-// Convert documents to string to be included in the prompt
+
 const docsToJson = (documents: DocumentInterface[]) =>
   JSON.stringify(documents);
-// end::docsToJson[]
 
-// tag::function[]
+
+export interface AgentToolInput {
+  input: string;
+  rephrasedQuestion: string;
+}
+
+
 export default async function initVectorRetrievalChain(
   llm: BaseLanguageModel,
   embeddings: Embeddings
 ): Promise<Runnable<AgentToolInput, string>> {
-  // TODO: Create vector store instance
-  // const vectorStore = ...
-  // TODO: Initialize a retriever wrapper around the vector store
-  // const vectorStoreRetriever = ...
-  // TODO: Initialize Answer chain
-  // const answerChain = ...
-  // TODO: Return chain
-  // return RunnablePassthrough.assign( ... )
+  
+  const vectorStore = await initVectorStore(embeddings);
+  const vectorStoreRetriever = vectorStore.asRetriever(5);
+
+  
+  const answerChain = initGenerateAnswerChain(llm);
+  return RunnablePassthrough.assign({
+    documents: new RunnablePick("rephrasedQuestion").pipe(vectorStoreRetriever),
+  })
+    .assign({
+      ids: new RunnablePick("documents").pipe(extractDocumentIds),
+      context: new RunnablePick("documents").pipe(docsToJson),
+    })
+    .assign({
+      output: (input: RetrievalChainThroughput) =>
+        answerChain.invoke({
+          question: input.rephrasedQuestion,
+          context: input.context,
+        }),
+    })
+    .assign({
+      responseId: async (input: RetrievalChainThroughput, options) =>
+        saveHistory(
+          options?.configurable?.sessionId,
+          "vector",
+          input.input,
+          input.rephrasedQuestion,
+          input.output,
+          input.ids
+        ),
+    })
+    .pick("output");
 }
-// end::function[]
